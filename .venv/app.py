ed90547f4674a746e94a03771f12406a647e5ea0
Eduardo Santos <santosdeassis10@gmail.com>
2024-03-06 10:45:06 -0300

    Inicio do projeto, implementação de rotas, e extend do html base.

--- .venv/app.py
+++ .venv/app.py
@@ -12,6 +12,7 @@ def page_produto():
     itens = [{'id':1,'nome':"Celular",'cod_barra':"23721831",'preco':1200},
              {'id':2,'nome':"Notebook",'cod_barra':"5283463",'preco':3500},
              {'id':3,'nome':"Teclado",'cod_barra':"23762178",'preco':120},
-             {'id':4,'nome':"Monitor",'cod_barra':"127321838",'preco':800}]
+             {'id':4,'nome':"Monitor",'cod_barra':"127321838",'preco':800},
+             {'id':5,'nome':"Mouse",'cod_barra':"3223123",'preco':221}]
     return render_template("produtos.html",itens=itens)
     
