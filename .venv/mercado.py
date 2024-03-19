2a93f279ed296916314fb8fea39437f2543505c0
Eduardo Santos <santosdeassis10@gmail.com>
2024-03-19 08:49:43 -0300

    Adicionado Banco

--- .venv/mercado.py
+++ .venv/mercado.py
@@ -1,7 +1,18 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
-
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
+db.init_app(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(length = 30), nullable = False, unique = True)
+    preco = db.Column(db.Integer, nullable = False)
+    cod_barra = db.Column(db.String(length = 12),nullable = False, unique = True)
+    cod_barra = db.Column(db.String(length = 1024),nullable = False, unique = True)
+
 
 @app.route('/')
 def page_home():
