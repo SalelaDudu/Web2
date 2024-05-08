c914458ff0dc38014d82f234fe066ec5ba510e58
Eduardo Santos <santosdeassis10@gmail.com>
2024-05-08 11:58:33 -0300

    Login e registro prontos

--- app/app.py
+++ app/app.py
@@ -22,46 +22,41 @@ def index():
 
 @app.route("/authentication")
 def loginScreen():
-    flash('<>')
     return render_template("authentication.html")
     
 
 @app.route("/logar", methods=['POST'])
 def logar():
-    username = request.form['username']
-    password = request.form['password']
+    username = request.form['login_input']
+    password = request.form['login_password_input']
     senhaBanco = consulta(f'select password from login where username = "{username}"')
     if(len(senhaBanco) == 0):
         flash("Usuário não cadastrado! faça seu registro.")
-        return redirect("/registrar")
+        return redirect("/authentication#register")
     else:
         if(decryptar(senhaBanco[0][0],password)):
             flash("LOGADO!")
-            return redirect('/login')
+            return redirect('/authentication#logIn')
         else:
             flash("Usuário e/ou senha incorretos.")
-            return redirect('/login')
+            return redirect('/authentication#logIn')
         
-@app.route("/registrar")
-def registrar():
-    return render_template("register.html")
-
 @app.route('/registro', methods=['POST'])
 def registro():
 
-    username = request.form['username']
-    senha = request.form['password']
-    re_senha = request.form['re-password']
+    username = request.form['registro_input']
+    senha = request.form['registro_password_input']
+    re_senha = request.form['password_input_repeat']
     consultado = consulta(f'''select username from login where username = "{username}";''')
 
     if(len(consultado) > 0):
         flash('Nome de usuário já cadastrado')
-        return redirect('/registrar')
+        return redirect('/authentication#register')
     else:
         if(senha != re_senha):
             flash('As senhas não batem!')
-            return redirect('/registrar')
+            return redirect('/authentication#register')
         else:
             pw_hash = encryptar(senha)
             registerLogin(username,pw_hash)
-            return redirect('/login')
+            return redirect('/authentication#logIn')
